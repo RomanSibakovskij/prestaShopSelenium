1bc0bfbd077bcfd5e029ef3de75cdbb5e71a1cec
RomanSibakovskij <j4023958@gmail.com>
2024-08-04 09:47:18 +0300

    Add women clothes page/test sheets

---
+++ src/main/java/lt/techin/example/RegisteredUserWomenClothesPage.java
@@ -0,0 +1,9 @@
+package lt.techin.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisteredUserWomenClothesPage extends BasePage{
+    public RegisteredUserWomenClothesPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/lt/techin/example/RegisteredUserWomenClothesPageTest.java
@@ -0,0 +1,6 @@
+package lt.techin.example;
+
+public class RegisteredUserWomenClothesPageTest extends TestMethods{
+
+    
+}
