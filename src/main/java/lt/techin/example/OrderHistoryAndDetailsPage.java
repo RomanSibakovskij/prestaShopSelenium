f5ce52663ec6fc081b0ec1a2190b74c62238de6f
RomanSibakovskij <j4023958@gmail.com>
2024-08-06 10:35:21 +0300

    Add empty order history and details page/test sheets

---
+++ src/main/java/lt/techin/example/OrderHistoryAndDetailsPage.java
@@ -0,0 +1,9 @@
+package lt.techin.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrderHistoryAndDetailsPage extends BasePage{
+    public OrderHistoryAndDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+}
