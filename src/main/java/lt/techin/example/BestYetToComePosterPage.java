c1fcb712009d88121b09f6a950dfd25617193416
RomanSibakovskij <j4023958@gmail.com>
2024-08-06 11:14:25 +0300

    Add empty BestYetToCome page sheet

---
+++ src/main/java/lt/techin/example/BestYetToComePosterPage.java
@@ -0,0 +1,9 @@
+package lt.techin.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class BestYetToComePosterPage extends BasePage{
+    public BestYetToComePosterPage(WebDriver driver) {
+        super(driver);
+    }
+}
