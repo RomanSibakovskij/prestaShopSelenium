96a5f9a89b86f2647a6516d179c1b8c0281965ac
RomanSibakovskij <j4023958@gmail.com>
2024-08-09 13:39:45 +0300

    Add display tags for wishlist page tests

--- src/test/java/lt/techin/example/RegisteredUserWishListPageTest.java
+++ src/test/java/lt/techin/example/RegisteredUserWishListPageTest.java
@@ -1,5 +1,6 @@
 package lt.techin.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 057 -> navigate to all products page test
     @Test
+    @DisplayName("User Navigation To All Products Page")
     @Tag("Navigate_To_All_Products_Page")
     void navigateToAllProductsPageTest(){
         clickOnSignUpTest();
@@ -25,6 +27,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058 -> add "Hummingbird printed t-shirt" into wishlist test
     @Test
+    @DisplayName("Add 'Humming Bird' T-shirt Into Wishlist")
     @Tag("Add_TShirt_To_Wishlist")
     void addTShirtToWishListTest(){
         clickOnSignUpTest();
@@ -43,6 +46,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058a -> add "Hummingbird printed sweater" into wishlist test
     @Test
+    @DisplayName("Add 'Humming Bird' Sweater Into Wishlist")
     @Tag("Add_Sweater_To_Wishlist")
     void addSweaterToWishListTest(){
         clickOnSignUpTest();
@@ -61,6 +65,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058b -> add "Best Is Yet To Come" picture into wishlist test
     @Test
+    @DisplayName("Add 'Best Is Yet To Come' Poster Into Wishlist")
     @Tag("Add_Pictures_To_Wishlist")
     void addBestYetToComePictureToWishListTest(){
         clickOnSignUpTest();
@@ -79,6 +84,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058c -> add "Adventure Begins" picture into wishlist test
     @Test
+    @DisplayName("Add 'The Adventure Begins' Poster Into Wishlist")
     @Tag("Add_Pictures_To_Wishlist")
     void addAdventureBeginsPictureToWishListTest(){
         clickOnSignUpTest();
@@ -95,8 +101,9 @@ public class RegisteredUserWishListPageTest extends TestMethods{
         addPictureAdventureBeginsWishListButtonTest();
     }
 
-    //Test 058d -> add "Adventure Begins" picture into wishlist test (My wishlist inner link doesn't click)
+    //Test 058d -> add "Today Is A Good Day" picture into wishlist test (My wishlist inner link doesn't click)
     @Test
+    @DisplayName("Add 'Today Is A Good Day' Poster Into Wishlist")
     @Tag("Add_Pictures_To_Wishlist")
     void addGoodDayPictureToWishListTest(){
         clickOnSignUpTest();
@@ -115,6 +122,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058e -> add "Best Yet To Come" mug into wishlist test
     @Test
+    @DisplayName("Add 'Best Yet To Come' Mug Into Wishlist")
     @Tag("Add_Mugs_To_Wishlist")
     void addBestYetMugToWishListTest(){
         clickOnSignUpTest();
@@ -133,6 +141,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058f -> add "Adventure Begins" mug into wishlist test
     @Test
+    @DisplayName("Add 'The Adventure Begins' Mug Into Wishlist")
     @Tag("Add_Mugs_To_Wishlist")
     void addAdventureBeginsMugToWishListTest(){
         clickOnSignUpTest();
@@ -151,6 +160,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058g -> add "Today Is A Good Day" mug into wishlist test
     @Test
+    @DisplayName("Add 'today Is A Good Day' Mug Into Wishlist")
     @Tag("Add_Mugs_To_Wishlist")
     void addGoodDayMugToWishListTest(){
         clickOnSignUpTest();
@@ -169,6 +179,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058h -> add "Mountain fox" cushion into wishlist test
     @Test
+    @DisplayName("Add 'Mountain Fox' Cushion Into Wishlist")
     @Tag("Add_Cushions_To_Wishlist")
     void addCushionFoxToWishListTest(){
         clickOnSignUpTest();
@@ -187,6 +198,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058i -> add "Brown Bear" cushion into wishlist test
     @Test
+    @DisplayName("Add 'Brown Bear' Cushion Into Wishlist")
     @Tag("Add_Cushions_To_Wishlist")
     void addCushionBrownBearToWishListTest(){
         clickOnSignUpTest();
@@ -205,6 +217,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058j -> add "Humming Bird" cushion into wishlist test
     @Test
+    @DisplayName("Add 'Humming Bird' Cushion Into Wishlist")
     @Tag("Add_Cushions_To_Wishlist")
     void addCushionHummingBirdToWishListTest(){
         clickOnSignUpTest();
@@ -223,6 +236,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058k -> add "Mountain Bird" vector picture into wishlist test
     @Test
+    @DisplayName("Add 'Mountain Fox' Graphics Into Wishlist")
     @Tag("Add_Graphics_To_Wishlist")
     void addVectorFoxToWishListTest(){
         clickOnSignUpTest();
@@ -241,6 +255,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058l -> add "Brown Bear" vector picture into wishlist test  (My wishlist inner link cannot be found)
     @Test
+    @DisplayName("Add 'Brown Bear' Graphics Into Wishlist")
     @Tag("Add_Graphics_To_Wishlist")
     void addVectorBrownBearToWishListTest(){
         clickOnSignUpTest();
@@ -259,6 +274,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058m -> add "Humming Bird" vector picture into wishlist test (My wishlist inner link cannot be found)
     @Test
+    @DisplayName("Add 'Humming Bird' Graphics Into Wishlist")
     @Tag("Add_Graphics_To_Wishlist")
     void addVectorHummingBirdToWishListTest(){
         clickOnSignUpTest();
@@ -277,6 +293,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058m -> add "Mug and Poster" pack into wishlist test
     @Test
+    @DisplayName("Add Mug and Poster Pack Into Wishlist")
     @Tag("Add_Pack_To_Wishlist")
     void addPackMugAndPosterToWishListTest(){
         clickOnSignUpTest();
@@ -295,6 +312,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058n -> add "Mountain Fox" notebook into wishlist test
     @Test
+    @DisplayName("Add 'Mountain Fox' Notebook Into Wishlist")
     @Tag("Add_Notebooks_To_Wishlist")
     void addNotebookFoxToWishListTest(){
         clickOnSignUpTest();
@@ -313,6 +331,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058o -> add "Brown Bear" notebook into wishlist test
     @Test
+    @DisplayName("Add 'Brown Bear' Notebook Into Wishlist")
     @Tag("Add_Notebooks_To_Wishlist")
     void addNotebookBearToWishListTest(){
         clickOnSignUpTest();
@@ -331,6 +350,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058p -> add "Humming Bird" notebook into wishlist test
     @Test
+    @DisplayName("Add 'Humming Bird' Notebook Into Wishlist")
     @Tag("Add_Notebooks_To_Wishlist")
     void addNotebookBirdToWishListTest(){
         clickOnSignUpTest();
@@ -349,6 +369,7 @@ public class RegisteredUserWishListPageTest extends TestMethods{
 
     //Test 058q -> add "Custom Text" mug into wishlist test
     @Test
+    @DisplayName("Add 'Custom Text' Mug Into Wishlist")
     @Tag("Add_Mugs_To_Wishlist")
     void addMugCustomTextToWishListTest(){
         clickOnSignUpTest();
