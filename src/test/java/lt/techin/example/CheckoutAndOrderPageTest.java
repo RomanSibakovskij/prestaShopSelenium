982d13463ab1e3d9068e1ff1451c62484bdf20db
RomanSibakovskij <j4023958@gmail.com>
2024-08-09 13:55:50 +0300

    Add display tags for checkoutand order page tests

--- src/test/java/lt/techin/example/CheckoutAndOrderPageTest.java
+++ src/test/java/lt/techin/example/CheckoutAndOrderPageTest.java
@@ -1,5 +1,6 @@
 package lt.techin.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ public class CheckoutAndOrderPageTest extends TestMethods{
 
     //Test 062 -> order and history test (link test) -> pay by check option
     @Test
+    @DisplayName("Checkout And Order Of 'Best Yet To Come' Poster (pay by check)")
     @Tag("Single_Product_Order_And_History_Link")
     void singleProductOrderAndHistoryLinkTest(){
         clickOnSignUpTest();
@@ -25,6 +27,7 @@ public class CheckoutAndOrderPageTest extends TestMethods{
 
     //Test 062a -> order and history test (box icon test) -> pay by bank wire option
     @Test
+    @DisplayName("Checkout And Order Of 'Best Yet To Come' poster (pay by bank wire)")
     @Tag("Single_Product_Order_And_History_Box_Icon")
     void singleProductOrderAndHistoryBoxIconTest(){
         clickOnSignUpTest();
@@ -43,6 +46,7 @@ public class CheckoutAndOrderPageTest extends TestMethods{
 
     //Test 062b -> order and history test (box icon test) -> pay by cash option
     @Test
+    @DisplayName("Checkout And Order Of 'Best Yet To Come' Poster (pay by cash)")
     @Tag("Single_Product_Order_And_History_Box_Icon")
     void singleProductOrderAndHistoryPayByCashTest(){
         clickOnSignUpTest();
@@ -61,6 +65,7 @@ public class CheckoutAndOrderPageTest extends TestMethods{
 
     //Test 063 -> add product into shopping cart and remove it test (the icon needs to be clicked repeatedly if more than one product (quantity of the same) is present)
     @Test
+    @DisplayName("Add And Remove The 'Best Yet To Come' Poster From Cart")
     @Tag("Single_Product_Add_And_Remove_From_Cart")
     void addAndRemoveProductFromCartTest(){
         clickOnSignUpTest();
