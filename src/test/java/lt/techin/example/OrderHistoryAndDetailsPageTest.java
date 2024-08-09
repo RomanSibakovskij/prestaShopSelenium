773a6604964f12728ffe3b6f43e75bd1274eec12
RomanSibakovskij <j4023958@gmail.com>
2024-08-09 13:50:22 +0300

    Add display tags for order history and details page tests

--- src/test/java/lt/techin/example/OrderHistoryAndDetailsPageTest.java
+++ src/test/java/lt/techin/example/OrderHistoryAndDetailsPageTest.java
@@ -6,6 +6,7 @@ public class OrderHistoryAndDetailsPageTest extends TestMethods{
 
     //Test 064 -> order and history test of a single product
     @Test
+    @DisplayName("Order History And Details of a 'The Best is Yet To Come' Poster")
     @Tag("Order_History_Of_Single_Product")
     void singleProductOrderAndHistoryPayByBankWireTest(){
         clickOnSignUpTest();
@@ -24,6 +25,7 @@ public class OrderHistoryAndDetailsPageTest extends TestMethods{
 
     //Test 065 -> send a message for a single product order
     @Test
+    @DisplayName("Send A Meesage For 'The Best is Yet To Come' Poster Order")
     @Tag("Send_Message_For_Single_Product_Order")
     void sendMessageForASingleProductOrderTest(){
         clickOnSignUpTest();
