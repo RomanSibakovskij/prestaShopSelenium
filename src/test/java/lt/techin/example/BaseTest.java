2fe4e9a909cee5886e396c40e3fa2282928427b0
RomanSibakovskij <j4023958@gmail.com>
2024-09-12 11:42:09 +0300

    Update BaseTest configuration

--- src/test/java/lt/techin/example/BaseTest.java
+++ src/test/java/lt/techin/example/BaseTest.java
@@ -6,14 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.sql.*;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-
 
 public class BaseTest {
     protected WebDriver driver;
-    protected Connection conn;
 
     @BeforeEach
     public void setUp() {
@@ -21,14 +16,7 @@ public class BaseTest {
         options.addArguments("--disable-search-engine-choice-screen");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("http://192.168.8.187");
-//        // set up database connection
-//        try {
-//            conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/userdb", "sa", "");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException("Failed to connect to the database", e);
-//        }
+        driver.get("http://192.168.8.194");
     }
 
     @AfterEach
@@ -39,13 +27,5 @@ public class BaseTest {
             throw new RuntimeException(e);
         }
         driver.quit();
-//	// terminate database connection
-//        if (conn != null) {
-//            try {
-//                conn.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 }
